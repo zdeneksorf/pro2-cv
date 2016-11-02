2d9b62ad7798a6f7279526b37dbc40412d8ac8af
zdeneksorf <zdenek.sorf@uhk.cz>
2016-11-02 13:08:52 +0100

    Vytvoreni metod a atributu ve tride Hrac, Vytvoreni tridy HraciPlocha

--- src/hra/Hrac.java
+++ src/hra/Hrac.java
@@ -1,6 +1,72 @@
 package hra;
 
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
 public class Hrac {
+	
+	public static final int SIRKA = 40;
+	public static final int VYSKA = 33;
+	
+	//velikost skoku hrace
+	private static final int KOEF_ZRYCHLENI = 1;
+	//rychlost padu hrace
+	private static final int KOEF_RYCHLOST = 2;
+	private BufferedImage img = null;
+	//pocatecni x-ova pozice hrace, nemeni se (hrac neskace dopredu)
+	private int x;
+	//pocatecni y-ova pozice hrace, meni se (hrac skace nahoru, dolu)
+	private int y;
+	private int rychlost;
+	
+	
+	public Hrac(BufferedImage img) {
+		this.img = img;
+		x = (HraciPlocha.SIRKA / 2) - (img.getWidth() / 2); 
+		y = HraciPlocha.VYSKA / 2;   //stred hraci plochy
+		rychlost = KOEF_RYCHLOST;
+	}
+	
+	// vola se po narazu do zdi, do kraje okna
+	public void reset(){
+		y = HraciPlocha.VYSKA / 2;
+		rychlost = KOEF_RYCHLOST;
+		
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	
+	public void skok(){
+		
+		rychlost = -17;
+	}
+	//zajistuje pohyb hrace
+	public void posun(){
+		rychlost = rychlost + KOEF_ZRYCHLENI;
+		y = y + rychlost;
+		
+	}
+	
+	public void paint(Graphics  g){
+		g.drawImage(img, x, y, null);
+		
+	}
+	
+	public int getVyskaHrace(){
+		return img.getHeight();
+		
+	}
+	// vraci pomyslny ctverec/obdelnik, ktery opisuje hrace
+	public Rectangle getMez(){
+		return new Rectangle(x, y, img.getWidth(), img.getHeight());
+		
+	}
 	
 	
 
---
+++ src/hra/HraciPlocha.java
@@ -0,0 +1,12 @@
+package hra;
+
+import javax.swing.JPanel;
+
+public class HraciPlocha extends JPanel {
+	
+	public static final int VYSKA = 800;
+	public static final int SIRKA = 600;
+	
+	
+
+}
